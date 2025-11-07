ba8d83afbc699529906a7afeb382fc84f8127559
zad <kimminkyeong0418@gmail.com>
2025-11-07 17:28:50 +0900

    build:초기 설정

---
+++ src/main/java/com/mink/rebproj/RebProjApplication.java
@@ -0,0 +1,13 @@
+package com.mink.rebproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mink/rebproj/RebProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mink.rebproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
